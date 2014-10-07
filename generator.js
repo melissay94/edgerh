c19556af0fe169fe63a6f8dddf1c4a563dc9efb7
kristenmills <kristen@kristen-mills.com>
2014-10-06 21:15:05 -0400

    It's been angulared

--- generator.js
+++ generator.js
@@ -1,55 +1,53 @@
 'use strict';
+(function(){
+  angular.module('generator', [])
+    .controller('GeneratorController', GeneratorController);
 
-var Generator = {
-  rules: {
-    i: 'y',
-    s: 'z',
-    tt: 'xt',
-    ohn: 'aughn',
-    o: 'au',
-    c: 'x',
-    en: 'aughn',
-    an: 'aughn',
-    r: 'rh',
-    ph: 'f',
-    f: 'ph',
-    ew: 'u',
-    y: 'i',
-    k: 'qu'
-  },
+  function GeneratorController($scope){
+    var rules = {
+      i: 'y',
+      s: 'z',
+      tt: 'xt',
+      ohn: 'aughn',
+      o: 'au',
+      c: 'x',
+      en: 'aughn',
+      an: 'aughn',
+      r: 'rh',
+      ph: 'f',
+      f: 'ph',
+      ew: 'u',
+      y: 'i',
+      k: 'qu'
+    };
 
-  generate: function(name) {
-    var newName = "";
-    while(name.length > 0) {
-      var found = false;
-      for(var rule in this.rules) {
-        if(name.indexOf(rule) === 0){
-          newName += this.rules[rule];
-          name = name.replace(rule, '');
-          found = true;
-          break;
+    $scope.name = '';
+
+    function generate(name) {
+      var newName = "";
+      while(name.length > 0) {
+        var found = false;
+        for(var rule in rules) {
+          if(name.indexOf(rule) === 0){
+            newName += rules[rule];
+            name = name.replace(rule, '');
+            found = true;
+            break;
+          }
+        }
+        if(name[0] === name[1]){
+          name = name.slice(1);
+        }
+        if(!found){
+          newName += name[0];
+          name = name.replace(name[0], '');
         }
       }
-      if(name[0] === name[1]){
-        name = name.slice(1);
-      }
-      if(!found){
-        newName += name[0];
-        name = name.replace(name[0], '');
-      }
+      return  newName.charAt(0).toUpperCase() + newName.slice(1);
     }
-    return  newName.charAt(0).toUpperCase() + newName.slice(1);
-  },
 
-  buttonClicked: function() {
-    var input = document.querySelector('#name');
-    var name = input.value.toLowerCase();
-
-    document.querySelector('.title span').textContent = this.generate(name);
-  },
-  keyup: function(event) {
-    if(event.keyCode === 13) {
-      this.buttonClicked();
-    }
+    $scope.$watch('name', function(newValue, oldValue) {
+      $scope.display = generate(newValue);
+    });
   }
-};
+})();
